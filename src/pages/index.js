49ba7497ae01fedb70dc4fb1cc9c0e4acae6d152
Jack Shepherd <jackwshepherd@gmail.com>
2022-07-01 09:49:40 +0100

    fix broken links

--- src/pages/index.js
+++ src/pages/index.js
@@ -16,7 +16,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/basics">
+            to="./basics">
             Become a contributor &nbsp;&nbsp;👋
           </Link>
         </div>
